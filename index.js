6edda5ae42d8877afba3e6de7f12743cae1ca7d0
Michael Boroski <mjboroski@gmail.com>
2018-04-04 23:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if(getCart.length==0){
+  if(getCart().length==0){
     return "Your shopping cart is empty.";
   }
-  if(getCart.length==1){
+  if(getCart().length==1){
     return "In your cart, you have "+itemName+" at $"+itemPrice;
   }
   else{
     var tempString = 'In your cart, you have';
-    for(var i=0;i<getCart.length-1;i++){
+    for(var i=0;i<getCart().length-1;i++){
       tempString=tempString+' '+itemName+' at '+itemPrice+',';
     }
-    tempString=tempstring+' and'+getCart[getCart.length-1].itemName+' at $'+getCart[getCart.length-1].itemPrice+'.';
+    tempString=tempstring+' and'+getCart()[getCart().length-1].itemName+' at $'+getCart()[getCart().length-1].itemPrice+'.';
     return tempstring;
   }
 }
