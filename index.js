3d1188019b89ee2f8752e660dd273731103ecda6
Michael Boroski <mjboroski@gmail.com>
2018-04-04 23:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if(getCart.length=0){
+  if(getCart.length===0){
     return "Your shopping cart is empty.";
   }
-  if(getCart.length=1){
+  if(getCart.length===1){
     return "In your cart, you have "+itemName+" at $"+itemPrice;
   }
   else{
