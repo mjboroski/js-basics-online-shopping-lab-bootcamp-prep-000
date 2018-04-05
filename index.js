d090f1951b3625a91c270f24beafdb515d29962d
Michael Boroski <mjboroski@gmail.com>
2018-04-05 00:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item){
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber==0){
+  if (cardNumber==F){
     return "Sorry, we don't have a credit card on file for you."
   }else{
     var string = "Your total cost is $"+total()+", which will be charged to the card "+cardNumber+"."
